5751aff49a1491d5011a40db1034c467591e7f4c
Davide Finati <d.finati@campus.unimib.it>
2019-10-31 19:01:08 +0100

    Aggiunti unit test

--- IntranetAziendale/UnitTests/TestDB.cs
+++ IntranetAziendale/UnitTests/TestDB.cs
@@ -9,113 +9,26 @@ namespace Tests
 {
     public class TestDB
     {
-        int idNewDip = 0;
-        int idNewConv = 0;
-
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        /// <summary>
-        /// TEST CRUD DIPENDENTE
-        /// </summary>
-
-
         [Test]
-        public void TestAddDipendente()
+        public void UnitTestDescrizione()
         {
-            DipendenteDMController controller = new DipendenteDMController();
+            Convenzione testc1 = new Convenzione();
+            testc1.Titolo = "testTitolo";
+            testc1.Descrizione = "testDescrizione";
 
-            RequestAddDipendente d = new RequestAddDipendente()
-            {
-                Cognome = "Verdi",
-                Nome = "Carlo",
-                Username = "carlo.verdi",
-                Psw = "1234",
-                IDRuolo = 3
-            };
-
-            idNewDip = controller.Add(d);
-            
-            Assert.Greater(idNewDip, 0);
+            Assert.AreEqual(testc1.Titolo, "testTitolo");
+            Assert.AreEqual(testc1.Descrizione, "testDescrizione");
         }
 
         [Test]
-        public void TestGetDipendenti()
+        public void UnitTestDipendente()
         {
-            DipendenteDMController controller = new DipendenteDMController();
-            List<ResponseGetDipendenti> listDip = controller.Get();
+            Dipendente testd1 = new Dipendente();
+            testd1.Nome = "testNome";
+            testd1.Cognome = "testCognome";
 
-            Assert.GreaterOrEqual(idNewDip, listDip.Count);
-        }
-
-        [Test]
-        public void TestGetDipendenteByID()
-        {
-            DipendenteDMController controller = new DipendenteDMController();
-            Dipendente dip = controller.GetByID(1);
-
-            Assert.AreEqual(dip.Cognome, "Bianchi");
-            Assert.AreEqual(dip.Nome, "Marco");
-        }
-
-        [Test]
-        public void TestDeleteDipendente()
-        {
-            DipendenteDMController controller = new DipendenteDMController();
-
-            bool isDelete = controller.Delete(idNewDip);
-
-            Assert.AreEqual(isDelete, true);
-        }
-
-        /// <summary>
-        /// TEST CRUD CONVENZIONE
-        /// </summary>
-
-        [Test]
-        public void TestAddConvenzione()
-        {
-            ConvenzioneDMController controller = new ConvenzioneDMController();
-
-            RequestAddConvenzione d = new RequestAddConvenzione()
-            {
-                Titolo = "Sconto parcheggio",
-                Descrizione = "Lorem ipsum"
-            };
-
-            idNewConv = controller.Add(d);
-
-            Assert.Greater(idNewConv, 0);
-        }
-
-        [Test]
-        public void TestGetConvenzioni()
-        {
-            ConvenzioneDMController controller = new ConvenzioneDMController();
-            List<Convenzione> listCon = controller.Get();
-
-            Assert.GreaterOrEqual(idNewConv, listCon.Count);
-        }
-
-        [Test]
-        public void TestGetConvenzioneByID()
-        {
-            ConvenzioneDMController c = new ConvenzioneDMController();
-            Convenzione dip = c.GetByID(1);
-
-            Assert.AreEqual(dip.Titolo, "Sconto Auto");
-        }
-
-        [Test]
-        public void TestDeleteConvenzione()
-        {
-            ConvenzioneDMController controller = new ConvenzioneDMController();
-
-            bool isDelete = controller.Delete(idNewConv);
-
-            Assert.AreEqual(isDelete, true);
+            Assert.AreEqual(testd1.Nome, "testNome");
+            Assert.AreEqual(testd1.Cognome, "testCognome");
         }
     }
 }
